cb68a035db4d565cdbc1f9c0f1e26e039010c6bc
Emiliano Colin-Diaz <emiliano.colin-diaz@yale.edu>
2023-11-12 03:04:48 -0500

    trying to fix

--- pages/app.py
+++ pages/app.py
@@ -154,4 +154,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         migrate.init_app(app, db)
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
+
